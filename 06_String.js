9d53541d83ad3de7748ef4d628b145e1d8c21840
Shoukat-Ali123 <143267338+Shoukat-Ali123@users.noreply.github.com>
2023-12-05 22:47:22 +0500

    Add files via upload

---
+++ 06_String.js
@@ -0,0 +1,35 @@
+// let name= "Shoukat"
+// let repocount = 50
+
+// //console.log(`Hello this is ${name} and my repositry count on Github is ${repocount}`);
+
+
+let gameName= new String ('  Magsi Sahab   ')
+// console.log(gameName.includes('Magsi'))
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.indexOf('a'));
+// console.log(gameName.charAt(3));
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(-8,-4));
+
+// const another = "   Name   "
+// console.log(another);
+// console.log(another.trimStart());
+// const url = "http//google%20.com"
+// console.log(url.replace("%20" ,"-"));
+
+// let a ="Shoukat-Ali-Magsi"
+// console.log(a.split('-'));
+// let b="this"
+// console.log(b.concat("Hello", "World"));
+
+// let a ="This is String"
+// console.log(a.endsWith('String',));
+// console.log(a.repeat(3));
+
+let a = new String('This');
+console.log(a);
+console.log(a.valueOf());
